b7cf12e271d8ed7f6ca6cf708891cc7cacfa8633
Andrey_Schulz <andreygorb040693@gmail.com>
2023-02-28 16:26:45 +0200

    comments

--- Screens/Main/CommentsScreen/CommentsScreen.js
+++ Screens/Main/CommentsScreen/CommentsScreen.js
@@ -1,3 +1,99 @@
-const CommentsScreen = () => {};
+import { useEffect, useState } from "react";
+import {
+  FlatList,
+  Image,
+  Keyboard,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import Octicons from "react-native-vector-icons/Octicons";
+import { useSelector } from "react-redux";
+import { db } from "../../../firebase/config";
+import {
+  collection,
+  addDoc,
+  query,
+  onSnapshot,
+  orderBy,
+  collectionGroup,
+} from "firebase/firestore";
+import { styles } from "./CommentsScreenStyled";
+
+const CommentsScreen = ({ route }) => {
+  const [comment, setComment] = useState("");
+  const [allComments, setAllComments] = useState([]);
+  const nickname = useSelector((state) => state.auth.displayName);
+  const { postId, photoURL } = route.params;
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "posts", postId, "comments"),
+      orderBy("dateTime")
+    );
+    const unsub = onSnapshot(q, (data) => {
+      const fetchedComments = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+
+      setAllComments(fetchedComments);
+      Keyboard.dismiss();
+    });
+
+    return () => unsub();
+  }, []);
+
+  async function sendComment() {
+    const postToUpdateRef = collectionGroup(db, "posts", postId, "comments");
+
+    await addDoc(postToUpdateRef, {
+      nickname,
+      comment,
+
+      dateTime: new Date(),
+    });
+
+    setComment("");
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 1 }}>
+        <Image source={{ uri: photoURL }} style={styles.image} />
+        <FlatList
+          data={allComments}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style={styles.commentBox}>
+              <Image source={{ uri: avatarURL }} style={styles.avatar} />
+              <View style={styles.messageBox}>
+                <Text style={styles.message}>{item.comment}</Text>
+                <Text style={styles.date}>
+                  {dateFormatter(item.dateTime.seconds)}
+                </Text>
+              </View>
+            </View>
+          )}
+        />
+      </View>
+      <View style={{ paddingTop: 8 }}>
+        <TextInput
+          style={styles.input}
+          value={comment}
+          placeholder="Комментировать..."
+          placeholderTextColor={"#BDBDBD"}
+          onChangeText={(text) => setComment(text)}
+        />
+
+        <Pressable style={styles.sendBtn} onPress={sendComment}>
+          <Octicons name="arrow-up" size={20} color="#fff" />
+        </Pressable>
+      </View>
+    </View>
+  );
+};
 
 export default CommentsScreen;
--- Screens/Main/CommentsScreen/CommentsScreenStyled.js
+++ Screens/Main/CommentsScreen/CommentsScreenStyled.js
@@ -1,3 +1,68 @@
 import { StyleSheet, Platform } from "react-native";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 32,
+    paddingBottom: 16,
+    justifyContent: "space-between",
+    backgroundColor: "#fff",
+  },
+  image: {
+    width: "100%",
+    height: 240,
+    borderRadius: 8,
+    marginBottom: 8,
+  },
+  sendBtn: {
+    position: "absolute",
+    right: 8,
+    top: 16,
+    height: 34,
+    width: 34,
+    backgroundColor: "#FF6C00",
+    borderRadius: 34,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    height: 50,
+    padding: 16,
+    borderWidth: 1,
+    borderRadius: 100,
+    borderColor: "#E8E8E8",
+    backgroundColor: "#F6F6F6",
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  commentBox: {
+    flexDirection: "row",
+    marginBottom: 24,
+  },
+  avatar: {
+    width: 36,
+    height: 36,
+    borderRadius: 28,
+    marginRight: 16,
+  },
+  messageBox: {
+    padding: 16,
+    backgroundColor: "#00000008",
+    borderRadius: 6,
+    borderTopLeftRadius: 0,
+  },
+  message: {
+    marginBottom: 8,
+    color: "#212121",
+    fontFamily: "Roboto400",
+    fontSize: 13,
+    lineHeight: 18,
+  },
+  date: {
+    color: "#BDBDBD",
+    fontFamily: "Roboto400",
+    fontSize: 10,
+    lineHeight: 12,
+  },
+});
